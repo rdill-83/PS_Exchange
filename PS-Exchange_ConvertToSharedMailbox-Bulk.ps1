6b0b84accf2c32f7397932c98a538ab94f541a6e
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-06-19 23:50:28 -0700

    Create PS-Exchange_ConvertToSharedMailbox-Bulk.ps1

---
+++ PS-Exchange_ConvertToSharedMailbox-Bulk.ps1
@@ -0,0 +1,18 @@
+# PS-Exchange_ConvertToSharedMailbox-Bulk.ps1
+
+## Convert CSV List of Users to Shared Mailboxes
+
+### Note - CSV Needs Header w/ 'mailbox'
+
+
+$mailboxes = Import-CSV -Path .\<your-file>.csv
+
+### Foreach-Object Method:
+$Mailboxes | foreach-object {Get-Mailbox -Identity $_.mailbox}
+
+
+### Convert To Shared Mailbox - WhatIf Sanity Check:
+$Mailboxes | foreach-object {Get-Mailbox -Identity $_.mailbox | Set-Mailbox -Type Shared -WhatIf}
+
+### Convert To Shared Mailbox - Bulk:
+$mailboxes | Foreach-Object {Get-Mailbox -Identity $_.Mailbox | Set-Mailbox -Type Shared}
