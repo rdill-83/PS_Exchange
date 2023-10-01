ec15ede2cd2573c50b559292ac4cfc0f3b5713ff
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-10-01 11:38:29 -0700

    23-10-01 Commit

--- PS-Exchange_Shared-Mailbox_Access-Report.ps1
+++ PS-Exchange_Shared-Mailbox_Access-Report.ps1
@@ -1,4 +1,6 @@
-# ExchangeOnline - Shared Mailbox Access Report
+# ExchangeOnline - Shared Mailbox Access & Activity Reports
+
+##### Shared Mailbox - Access Report:
 
 # Retrieve Shared Mailboxes:
 $Mailboxes = Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited
@@ -9,3 +11,7 @@ $Mailboxes | Get-MailboxPermission | Where {$_.User -notLike '*self'} | Sort Ide
 # Run Shared Mailbox Access Report - CSV Output:
 $mailboxes | Get-MailboxPermission | Where {$_.User -notLike '*SELF'} | Select Identity,User,Deny,AccessRights,isInherited | Export-CSV -LiteralPath "C:\Exchange_SharedMailbox_AccessReport_$(Get-Date -f yyyy-MM-dd).csv" -NoTypeInformation
 
+##### Shared Mailbox - Activity Report:
+
+# Run Shared Mailbox Activity Report - Console Output:
+$mailboxes | Get-MailboxStatistics -ErrorAction SilentlyContinue | Sort LastUserLogonTime | Select DisplayName,PrimarySMTPAddress,LastLogonTime,TotalItemSize
