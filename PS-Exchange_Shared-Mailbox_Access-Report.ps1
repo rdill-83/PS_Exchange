fe8e3f4ac0887d96555e6f4e794d1f828e05f339
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-10-01 11:42:59 -0700

    23-10-01 Commit

--- PS-Exchange_Shared-Mailbox_Access-Report.ps1
+++ PS-Exchange_Shared-Mailbox_Access-Report.ps1
@@ -1,9 +1,12 @@
 # ExchangeOnline - Shared Mailbox Access & Activity Reports
 
-##### Shared Mailbox - Access Report:
-
 # Retrieve Shared Mailboxes:
 $Mailboxes = Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited
+
+# Variable used in both Access & Activity Reports
+
+
+##### Shared Mailbox - Access Report:
 
 # Run Shared Mailbox Access Report - Console Output:
 $Mailboxes | Get-MailboxPermission | Where {$_.User -notLike '*self'} | Sort Identity | FT Identity,User,Deny,AccessRights,isInherited
@@ -11,7 +14,11 @@ $Mailboxes | Get-MailboxPermission | Where {$_.User -notLike '*self'} | Sort Ide
 # Run Shared Mailbox Access Report - CSV Output:
 $mailboxes | Get-MailboxPermission | Where {$_.User -notLike '*SELF'} | Select Identity,User,Deny,AccessRights,isInherited | Export-CSV -LiteralPath "C:\Exchange_SharedMailbox_AccessReport_$(Get-Date -f yyyy-MM-dd).csv" -NoTypeInformation
 
+
 ##### Shared Mailbox - Activity Report:
 
 # Run Shared Mailbox Activity Report - Console Output:
 $mailboxes | Get-MailboxStatistics -ErrorAction SilentlyContinue | Sort LastUserLogonTime | Select DisplayName,PrimarySMTPAddress,LastLogonTime,TotalItemSize
+
+# Run Shared Mailbox Activity Report - CSV Output:
+$mailboxes | Get-MailboxStatistics -ErrorAction SilentlyContinue | Sort LastUserLogonTime | Select DisplayName,PrimarySMTPAddress,LastLogonTime,TotalItemSize | Export-CSV -LiteralPath "C:\SharedMailbox-ActivityReport_$(Get-Date -F yyyy-mm-dd).csv" -NoTypeInformation
